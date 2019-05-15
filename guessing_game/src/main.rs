1bd1aa22624cd289c818f9852f90cb22361e9d17
mateovelilla <teovelilla@gmail.com>
2019-05-15 16:44:19 -0500

    Processing a Guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,17 @@
+/*
+* de esta manera de importan las librerias en rust
+* a continuación utilizaremos la libreria std del core de rust
+* https://doc.rust-lang.org/std/io/index.html
+* TODO: Post sobre prelude, seria interesante hablar sobre esto.
+* con esto acabo en el titulo "Processing a Guess"
+*/
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivine el numero!");
+    println!("Por favor intente adivinar:");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+    .expect("Fallo la lectura de la variable");
+    println!("Usted ingreso: {}",guess);
 }
