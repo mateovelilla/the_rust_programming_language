8876916e006a797bf65bdbf4f76d2d6575e59333
mateoVelilla <teovelilla@gmail.com>
2021-07-10 21:01:38 -0500

    Finished guess game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,18 +1,31 @@
 // https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html#comparing-the-guess-to-the-secret-number
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
   println!("Guess the number!");
   let secret_number = rand::thread_rng().gen_range(1..101);
-
-  println!("The secret number is {}", secret_number);
-
-  println!("Please input your guess.");
-
-  let mut guess = String::new();
+  loop {
+    println!("Please input your guess.");
+  
+    let mut guess = String::new();
+      
+    io::stdin().read_line(&mut guess)
+      .expect("Failed to read line");
     
-  io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
-  println!("You guessed {}",guess);
+    let guess: u32 = match guess.trim().parse(){
+      Ok(num) => num,
+      Err(_) => continue,
+    };
+  
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too Small"),
+      Ordering::Greater => println!("Too great"),
+      Ordering::Equal => {
+        println!("You Win!");
+        break;
+      },
+    }
+  }
 }
