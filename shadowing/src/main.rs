5febe5abb488b923e1698d3fe019dc7ef4af5624
mateoVelilla <teovelilla@gmail.com>
2021-07-14 00:50:48 -0500

    Shadowing

---
+++ shadowing/src/main.rs
@@ -0,0 +1,10 @@
+// URL: https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html#variables-and-mutability
+fn main() {
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("space {}", spaces);
+}
