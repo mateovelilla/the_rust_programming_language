6ebd2fd2585fa666a8310bb89cbd239e76f380dd
mateovelilla <teovelilla@gmail.com>
2021-10-06 00:04:19 +0000

    Create error handling with unwrap_or_else

--- minigrep_03/src/main.rs
+++ minigrep_03/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 struct Config {
     query: String,
     filename: String,
@@ -17,7 +18,10 @@ impl Config {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| { // unwrap_or_else statement is to define some custom, non-panic error handling
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
