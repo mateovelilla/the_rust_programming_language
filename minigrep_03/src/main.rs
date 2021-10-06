36b036053bea9fe829dd559d1737518e4ac3c6aa
mateovelilla <teovelilla@gmail.com>
2021-10-06 00:09:53 +0000

    Split responsabity in main with minigrep_03

--- minigrep_03/src/main.rs
+++ minigrep_03/src/main.rs
@@ -15,6 +15,11 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -25,8 +30,6 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    run(config);
 }
+
